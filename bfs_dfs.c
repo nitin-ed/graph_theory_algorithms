490ae5d79d4178fea58c12594e793c70a3e2bca9
Nitin De <66069955+Nimo-GitHub@users.noreply.github.com>
2020-06-13 08:12:28 -0700

    Create bfs_dfs.c

---
+++ bfs_dfs.c
@@ -0,0 +1,196 @@
+//Library Definitions
+
+#include<stdio.h>
+#include<stddef.h>
+#include<stdlib.h>
+
+//Structures
+
+typedef struct node
+{
+    int data;
+    struct node *next;
+}node;
+
+typedef struct Graph
+{
+    int vertices;
+    int* visited;
+    struct node **adjlist;
+}Graph;
+
+typedef struct qode
+{
+    int data;
+    struct qode *next;
+}qode;
+
+//Function Declerations
+
+Graph* create_graph(int v);
+node* create_node(int data);
+qode* create_qode(int data);
+void enqueue(int data);
+int dequeue();
+int isEmpty();
+void add_edge(Graph *graph);
+Graph* create_adjlist();
+void display_graph(Graph *graph);
+void display_queue();
+void BFS(Graph* graph ,int start);
+
+//Global Variables
+
+qode* front;
+qode* rear;
+
+//Main Function
+
+void main()
+{
+    display_graph(create_adjlist());
+}
+
+//Functions Defintions count = 10
+
+Graph* create_graph(int v)
+{
+    Graph *new;
+    int i;
+    new = (Graph*)malloc(v*sizeof(Graph));
+    new->vertices = v;
+    new->visited = (int*)malloc(v*sizeof(int));
+    new->adjlist = (node**)malloc(v*sizeof(node*));
+    i=0;
+    for ( i = 0; i < v; i++)
+    {
+        new->visited[i] = 0;
+        new->adjlist[i] = NULL; 
+    }
+    return new;
+}
+
+node* create_node(int data)
+{
+    node *new;
+    new = (node*)malloc(sizeof(node));
+    new->data = data;
+    new->next = NULL;
+    return new;
+}
+
+qode* create_qode(int data)
+{
+    qode *new;
+    new = (qode*)malloc(sizeof(qode));
+    new->data = data;
+    new->next = NULL;
+    return new;
+}
+
+void enqueue(int data)
+{
+    qode *new,*temp;
+    if (front == NULL)
+    {
+        front = create_qode(data);
+        rear = front;
+    }
+    else
+    {
+        rear->next = new;
+        rear = new;
+    }
+}
+
+int dequeue()
+{
+    int value;
+    if(isEmpty()) return 0;
+    else if (front == rear)
+    {
+        value = front->data;
+        front = NULL;
+        return value;
+    }
+    else
+    {
+        value = front->data;
+        front = front->next;
+        return value;
+    }
+}
+
+int isEmpty()
+{
+    return(front == NULL);
+}
+
+void add_edge(Graph *graph)
+{
+    int v1 , v2;
+    node *newnode;
+    printf("Enter the vertices connected to the edge : ");
+    scanf("%d %d" , &v1 , &v2);
+    v1--; v2--;
+
+    //connecting v1 to v2
+    newnode = create_node(v2);
+    newnode->next = graph->adjlist[v1];
+    graph->adjlist[v1] = newnode;
+
+    //connecting v2 to v1
+    newnode = create_node(v1);
+    newnode->next = graph->adjlist[v2];
+    graph->adjlist[v2] = newnode; 
+}
+
+Graph* create_adjlist()
+{
+    int v , e , i;
+    printf("Enter total number of vertices in the graph : ");
+    scanf("%d" ,&v);
+    Graph *graph = create_graph(v);
+    printf("Enter number of edges to initialize the graph : ");
+    scanf("%d" , &e);
+    for ( i = 0; i < e; i++)
+    {
+        add_edge(graph);
+    }
+    return graph;
+}
+
+void display_graph(Graph *graph)
+{
+    int i;
+    for ( i = 0; i < graph->vertices; i++)
+    {
+        node *temp = graph->adjlist[i];
+        printf("%d : " , i + 1);
+        while (temp)
+        {
+            printf("-> %d " , temp->data + 1);
+            temp = temp->next;
+        }
+        printf("\n");
+    }
+    
+}
+
+void display_queue()
+{
+    qode *temp = front;
+    printf("\n[ ");
+    while(temp != NULL)
+    {
+        printf(" %d ",temp->data);
+        temp = temp->next;
+    }
+    printf(" ]\n");
+}
+
+void BFS(Graph* graph ,int start)
+{
+    
+}
+
